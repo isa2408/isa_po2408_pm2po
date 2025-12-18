14f40a5d8a02765518a144ab0b0b389c367c6b90
isa2408 <fless301008@gmail.com>
2025-12-18 16:49:51 +0500

    создан бот

---
+++ boy.py
@@ -0,0 +1,119 @@
+import telebot
+import database
+TOKEN = "8549479870:AAGa7lkFhGhteXXQJlSHXcySb_ucyvYw20w"
+
+bot = telebot.TeleBot(TOKEN)
+database.start_db()
+from telebot.types import ReplyKeyboardMarkup
+def gender_keyboard():
+    kb = ReplyKeyboardMarkup(resize_keyboard=True)
+    kb.add("👔 Мужское", "👗 Женское")
+    kb.add("👕 Unisex")
+    return kb
+
+user_cart = {}
+@bot.message_handler(commands=['add'])
+def add_to_cart(message):
+    args = message.text.split(maxsplit=1)
+
+    if len(args) < 2:
+        bot.send_message(message.chat.id, "Используй: /add Название")
+        return
+
+    item_name = args[1]
+
+    if message.chat.id not in user_cart:
+        user_cart[message.chat.id] = []
+
+    user_cart[message.chat.id].append(item_name)
+    bot.send_message(message.chat.id, f"🛒 {item_name} добавлен в корзину")
+
+@bot.message_handler(commands=['cart'])
+def show_cart(message):
+    cart = user_cart.get(message.chat.id, [])
+
+    if not cart:
+        bot.send_message(message.chat.id, "🛒 Корзина пуста")
+        return
+
+    text = "🛒 Ваша корзина:\n"
+    for item in cart:
+        text += f"• {item}\n"
+
+    bot.send_message(message.chat.id, text)
+
+# Команда /start
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(
+        message.chat.id,
+        "Привет! Я Telegram-бот\nНапиши что-нибудь "
+    )
+
+
+catalog = database.select_catalog()
+# Команда /catalog
+@bot.message_handler(commands=['catalog'])
+def choose_gender(message):
+    bot.send_message(
+        message.chat.id,
+        "Выберите категорию:",
+        reply_markup=gender_keyboard()
+    )
+
+@bot.message_handler(func=lambda m: m.text in ["👔 Мужское", "👗 Женское", "👕 Unisex"])
+def show_gender_catalog(message):
+
+    mapping = {
+        "👔 Мужское": "Men",
+        "👗 Женское": "Women",
+        "👕 Unisex": "Unisex"
+    }
+
+    items = database.select_catalog_by_gender(mapping[message.text])
+
+    if not items:
+        bot.send_message(message.chat.id, "Нет товаров")
+        return
+
+    text = "🛍 Товары:\n\n"
+    for name, price, gender, style in items:
+        text += (
+            f"👕 {name}\n"
+            f"💰 {price}\n"
+            f"🎨 {style}\n"
+            "────────────\n"
+        )
+
+    bot.send_message(message.chat.id, text)
+
+@bot.message_handler(commands=['style'])
+def style_search(message):
+    args = message.text.split(maxsplit=1)
+
+    if len(args) < 2:
+        bot.send_message(message.chat.id, "Пример: /style Casual")
+        return
+
+    items = database.select_by_style(args[1])
+
+    if not items:
+        bot.send_message(message.chat.id, "Ничего не найдено")
+        return
+
+    text = "🎨 Найдено:\n"
+    for name, price in items:
+        text += f"{name} — {price}\n"
+
+    bot.send_message(message.chat.id, text)
+ 
+
+# Ответ на любые сообщения
+@bot.message_handler(func=lambda message: True)
+def echo(message):
+    bot.send_message(
+        message.chat.id,
+        f"Ты написал: {message.text}"
+    )
+# Запуск бота
+bot.polling(none_stop=True)
--- database.py
+++ database.py
@@ -1,76 +1,60 @@
-# import psycopg2
-# from psycopg2 import OperationalError
-
-# DB_CONFIG = {
-#     "dbname": "your_db_name",
-#     "user": "postgres",
-#     "password": "your_password",
-#     "host": "localhost",
-#     "port": 5432
-# }
-
-# def get_connection():
-#     try:
-#         conn = psycopg2.connect(**DB_CONFIG)
-#         return conn
-#     except OperationalError as e:
-#         print("Ошибка подключения к базе данных:", e)
-#         return None
-
-
-# def create_table():
-#     conn = get_connection()
-#     if conn is None:
-#         return
-
-#     cursor = conn.cursor()
-
-#     cursor.execute("""
-#         CREATE TABLE IF NOT EXISTS students (
-#             id SERIAL PRIMARY KEY,
-#             full_name VARCHAR(100) NOT NULL,
-#             age INT CHECK (age > 0),
-#             email VARCHAR(100) UNIQUE
-#         );
-#     """)
-
-#     conn.commit()
-#     cursor.close()
-#     conn.close()
-
-
-# if __name__ == "__main__":
-#     create_table()
-#     print("Таблица успешно создана")
-
 import sqlite3
 
+def start_db(): # Создаем и подключаемся к базе данных
+    connection = sqlite3.connect('my_database.db')
+    cursor = connection.cursor()
+    return connection, cursor # возвращает кортеж (connection - подключение к бд, cursor - работа с бд)
 
-# Устанавливаем соединение с базой данных
-connection = sqlite3.connect('my_database.db')
-cursor = connection.cursor()
+def create_catalog_table(): # Создаем таблицу catalog, если ее нет
+    connection, cursor = start_db()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS catalog (
+    id INTEGER PRIMARY KEY,
+    nazvaniye TEXT NOT NULL,
+    sena INTEGER NOT NULL,
+    pol TEXT NOT NULL,
+    style TEXT NOT NULL,
+    picture TEXT
+    )
+    ''')
+    finalize_db(connection)
+def insert_sample_data_catalog(): # Вводит данные в таблицу catalog
+    connection, cursor = start_db()
+    cursor.execute('''INSERT INTO catalog (nazvaniye, sena, pol, style) VALUES
+    ('Futbolka', 20, 'Unisex', 'Casual'),
+    ('Jinsi', 50, 'Men', 'Casual'),
+    ('Platye', 80, 'Women', 'Formal')
+    ''')
+def select_catalog_by_gender(gender):
+    connection, cursor = start_db()
+    cursor.execute(
+    "SELECT nazvaniye, sena, pol, style FROM catalog WHERE pol = ?",
+    (gender,)
+    )
+    items = cursor.fetchall()
+    finalize_db(connection)
+    return items
 
-# Создаем таблицу Users
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS catalog (
-id INTEGER PRIMARY KEY,
-nazvaniye TEXT NOT NULL,
-sena INTEGER NOT NULL,
-pol TEXT NOT NULL,
-style TEXT NOT NULL
-)
-''')
-# cursor.execute('''INSERT INTO catalog (nazvaniye, sena, pol, style) VALUES
-# ('Futbolka', 20, 'Unisex', 'Casual'),
-# ('Jinsi', 50, 'Men', 'Casual'),
-# ('Platye', 80, 'Women', 'Formal')
-# ''')
-cursor.execute('''SELECT * FROM catalog''')
-catalog_items = cursor.fetchall()
-print(catalog_items)
-# Сохраняем изменения и закрываем соединение
-connection.commit()
-connection.close()
+def select_by_style(style):
+    connection, cursor = start_db()
+    cursor.execute(
+        "SELECT nazvaniye, sena FROM catalog WHERE style = ?",
+        (style,)
+    )
+    items = cursor.fetchall()
+    finalize_db(connection)
+    return items
+
+def select_catalog(): # Выводит данные из таблицы catalog
+    connection, cursor = start_db()
+    cursor.execute('''SELECT * FROM catalog''')
+    catalog_items = cursor.fetchall()
+    finalize_db(connection)
+    return catalog_items
+
+def finalize_db(connection): # Сохраняет и закрывает базу данных
+    connection.commit()
+    connection.close()
 
 
 
